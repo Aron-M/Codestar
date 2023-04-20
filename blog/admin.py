12e02d0e33ce4fe510dc2c80367bb0424f6e51e6
Aron-M <aron.moelich88@gmail.com>
2023-04-20 21:36:53 +0000

    adding Summernote to excerpts section of ADD POST

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
-class PostAdmin(SummernoteModelAdmin):
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
     summernote_fields = ('content')
-
