811e4af01431345b75ce825eecb9e5a351c20643
Aron-M <aron.moelich88@gmail.com>
2023-04-20 13:26:34 +0000

    changing names to codestar

--- i-think-therefor-i-blog/settings.py
+++ i-think-therefor-i-blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['i-think-therefor-i-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['Codestar', 'localhost']
 
 
 # Application definition
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'codestar.wsgi.application'
+WSGI_APPLICATION = 'Codestar.wsgi.application'
 
 
 # Database
