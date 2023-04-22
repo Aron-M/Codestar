be8105f5c78df7906db1aa0e1eae606afd2455bc
Aron-M <aron.moelich88@gmail.com>
2023-04-22 10:21:05 +0000

    deployment commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -77,3 +78,4 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
